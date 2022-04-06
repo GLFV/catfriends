109d0c25957db0fd2e1d09bed154888167ebc11c
GLFV <okolesnik753@gmail.com>
2022-04-07 00:38:38 +0300

    Updates

---
+++ precache-manifest.a4676a61c06608618be933826fd6b70b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "42b337db29208342d9f0",
+    "url": "/catfriends/static/css/main.e8eed3d9.chunk.css"
+  },
+  {
+    "revision": "42b337db29208342d9f0",
+    "url": "/catfriends/static/js/main.3ea5bda5.chunk.js"
+  },
+  {
+    "revision": "66b7e1568d85b12baeb4",
+    "url": "/catfriends/static/js/runtime~main.2a0eb0af.js"
+  },
+  {
+    "revision": "0262c2b651f6e826bc00",
+    "url": "/catfriends/static/css/2.54699cc7.chunk.css"
+  },
+  {
+    "revision": "0262c2b651f6e826bc00",
+    "url": "/catfriends/static/js/2.e2330bbf.chunk.js"
+  },
+  {
+    "revision": "a5ee85be2b9f2ed487af16a0ab33fde3",
+    "url": "/catfriends/static/media/BRUX-Regular.a5ee85be.woff2"
+  },
+  {
+    "revision": "76e787a0d47072f6c25474041c5bcfa7",
+    "url": "/catfriends/static/media/BRUX-Regular.76e787a0.woff"
+  },
+  {
+    "revision": "23f03bc2182396d72e036f9234654c77",
+    "url": "/catfriends/static/media/BRUX-Regular.23f03bc2.eot"
+  },
+  {
+    "revision": "01b08c6a3121a280dcc5add352ea055c",
+    "url": "/catfriends/static/media/BRUX-Regular.01b08c6a.ttf"
+  },
+  {
+    "revision": "7a8a3e7d9275b19dac622ed6f7930f68",
+    "url": "/catfriends/index.html"
+  }
+];
